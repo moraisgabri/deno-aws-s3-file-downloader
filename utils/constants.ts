5a54175b5f2f53b0472282df4cfcfb9c6cdb2607
Gabriel Morais <gabriel8azevedo@gmail.com>
2024-06-04 03:32:48 -0300

    fix: S3_BUCKET constant

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,5 @@
 import * as path from "https://deno.land/std@0.138.0/path/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
-import process from "node:process";
 
 export const DIRNAME = path.resolve(
   path.dirname(path.fromFileUrl(import.meta.url))
@@ -9,6 +8,6 @@ export const DIRNAME = path.resolve(
 export const INPUT = `${DIRNAME}/../inputs/main.tsv`;
 export const INPUT_RENAME = `${DIRNAME}/../inputs/rename.tsv`;
 export const OUTPUT = "output";
-export const S3_BUCKET = process.env.S3_BUCKET as string;
+export const S3_BUCKET = Deno.env.get("AWS_S3_BUCKET") as string;
 export const INPUT_S3_KEY = "s3Key";
 export const INPUT_FILE_NAME = "fileName";
