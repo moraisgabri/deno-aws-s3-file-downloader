a4dca8e355e0599afd345fa914753cdef30d4bcd
Gabriel Morais <gabriel8azevedo@gmail.com>
2024-06-04 03:29:36 -0300

    fix: remove from main convertRename until not implemented

--- download.ts
+++ download.ts
@@ -1,8 +1,7 @@
 // @deno-types="npm:@types/node"
 import "https://deno.land/x/dotenv/load.ts";
-import { convertRenameToJson } from "./functions/convert-rename-to-json.ts";
 import { resetOutputDir } from "./functions/reset-output-dir.ts";
-import { INPUT, INPUT_RENAME, OUTPUT } from "./utils/constants.ts";
+import { INPUT, OUTPUT } from "./utils/constants.ts";
 import { convertTsvToJson } from "./functions/convert-tsv-to-json.ts";
 import { downloadFile } from "./functions/download-file.ts";
 import { sleep } from "./functions/sleep.ts";
@@ -10,7 +9,6 @@ import { sleep } from "./functions/sleep.ts";
 resetOutputDir(OUTPUT);
 
 const items = convertTsvToJson(INPUT);
-convertRenameToJson(items, INPUT_RENAME);
 
 for (let i = 0; i < items.length; i++) {
   const { s3Key, fileName } = items[i];
